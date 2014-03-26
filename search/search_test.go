e70f1e50ac622134503965c6f49187bb885d8dc9
Dave Day <djd@golang.org>
2014-03-26 11:57:23 +1100

    appengine/search: add more detailed test cases.
    Change-Id: If6c017f87b390460e373abb77f04623bf7788018

--- search/search_test.go
+++ search/search_test.go
@@ -5,8 +5,56 @@
 package search
 
 import (
+	"reflect"
 	"testing"
+	"time"
+
+	"code.google.com/p/goprotobuf/proto"
+
+	pb "google.golang.org/appengine/internal/search"
 )
+
+type TestDoc struct {
+	String string
+	Atom   Atom
+	HTML   HTML
+	Float  float64
+	Time   time.Time
+}
+
+var (
+	float       = 3.14159
+	floatOut    = "3.14159e+00"
+	latitude    = 37.3894
+	longitude   = 122.0819
+	testString  = "foo<b>bar"
+	testTime    = time.Unix(1337324400, 0)
+	testTimeOut = "1337324400000"
+	searchDoc   = TestDoc{
+		testString,
+		Atom(testString),
+		HTML(testString),
+		float,
+		testTime,
+	}
+	protoFields = []*pb.Field{
+		newStringValueField("String", testString, pb.FieldValue_TEXT),
+		newStringValueField("Atom", testString, pb.FieldValue_ATOM),
+		newStringValueField("HTML", testString, pb.FieldValue_HTML),
+		newStringValueField("Float", floatOut, pb.FieldValue_NUMBER),
+		newStringValueField("Time", testTimeOut, pb.FieldValue_DATE),
+	}
+)
+
+func newStringValueField(name string, value string, valueType pb.FieldValue_ContentType) *pb.Field {
+	return &pb.Field{
+		Name: proto.String(name),
+		Value: &pb.FieldValue{
+			StringValue: proto.String(value),
+			Type:        valueType.Enum(),
+		},
+	}
+}
 
 func TestValidIndexNameOrDocID(t *testing.T) {
 	testCases := []struct {
@@ -28,5 +76,27 @@ func TestValidIndexNameOrDocID(t *testing.T) {
 		if got := validIndexNameOrDocID(tc.s); got != tc.want {
 			t.Errorf("%q: got %v, want %v", tc.s, got, tc.want)
 		}
+	}
+}
+
+func TestLoadFields(t *testing.T) {
+	got, want := TestDoc{}, searchDoc
+	err := loadFields(&got, protoFields)
+	if err != nil {
+		t.Fatalf("loadFields: %v", err)
+	}
+	if got != want {
+		t.Errorf("\ngot  %v\nwant %v", got, want)
+	}
+}
+
+func TestSaveFields(t *testing.T) {
+	got, err := saveFields(&searchDoc)
+	if err != nil {
+		t.Fatalf("saveFields: %v", err)
+	}
+	want := protoFields
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("\ngot  %v\nwant %v", got, want)
 	}
 }
