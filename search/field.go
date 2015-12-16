58c0e2a2044a8d1abd8dd1d97939cd74497d0806
Dave Day <djd@golang.org>
2015-12-16 21:03:11 +1100

    search: fix typo in comment (ISO 639 not 693).
    Change-Id: I8f360c9e72b3177bbbcd97aabad92dcf71798c40

--- search/field.go
+++ search/field.go
@@ -17,7 +17,7 @@ type Field struct {
 	//  - float64,
 	//  - GeoPoint.
 	Value interface{}
-	// Language is a two-letter ISO 693-1 code for the field's language,
+	// Language is a two-letter ISO 639-1 code for the field's language,
 	// defaulting to "en" if nothing is specified. It may only be specified for
 	// fields of type string and search.HTML.
 	Language string
