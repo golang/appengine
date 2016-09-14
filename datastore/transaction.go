9dfd5f12aa3e43735fb2b7ad28e7b66aa94eb6a7
Bo Song <songbo.sunny@gmail.com>
2016-09-14 13:34:10 -0700

    Update transaction.go

--- datastore/transaction.go
+++ datastore/transaction.go
@@ -70,6 +70,14 @@ func RunInTransaction(c context.Context, f func(tc context.Context) error, opts 
 	return ErrConcurrentTransaction
 }
 
+// IsInTransaction indicates whether the current context is a transaction context or not
+func IsInTransaction(c context.Context) bool {
+	if internal.transactionFromContext(c) != nil {
+		return true
+	}
+	return false
+}
+
 // TransactionOptions are the options for running a transaction.
 type TransactionOptions struct {
 	// XG is whether the transaction can cross multiple entity groups. In
