8d736f0e4ff70890d5e12525810b964bdd93c370
Luke Mccoy <lukemc@google.com>
2019-02-06 11:35:41 -0500

    splitting logic into a new file

---
+++ datastore/keycompatibility.go
@@ -0,0 +1,69 @@
+// Copyright 2011 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// This change adds additional compatibility to help customers
+// transition from google.golang.org/appengine/datastore (oldds) to cloud.google.com/go/datastore (newds).
+// Each lib (oldds and newds) contain functions Key.Encode() and Key.Decode(). These functions
+// create base64 representations of a json marshalled type of datastore keys.  Customers have been using
+// these encoded values to communicate between services in appengine.  The issue is that protobuf key types
+// change between oldds and newds making the corresponding base64 key strings incompatible.
+// Customer who attempt to upgrade to newds that use this pattern will fail.
+// keycompatibility.go placed in oldds enables forward compatibility of newds encoded keys.
+// An update to newds will also be necessary to enable backward compatibility.
+
+package datastore
+
+import (
+	"errors"
+
+	nds "cloud.google.com/go/datastore"
+	"google.golang.org/appengine/internal"
+)
+
+var errKeyConversion string = "Key conversions must be enabled in the application.\n  See <link> for more details."
+var convKey *keyConverter
+
+// EnableKeyConversion enables forward key conversation
+// abilities.  Calling this function in a single handler
+// will enable the feature for all handlers.  This function can be called in
+// the /_ah/start handler.
+// Support for key converstion.  Variable holds the appid
+// so that key conversion can retrieve it without a context.
+func EnableKeyConversion(ctx context) {
+	convKey = *keyConverter{
+		appid: internal.FullyQualifiedAppID(ctx),
+	}
+	return
+}
+
+// KeyConverter is the struct used to hold the appid
+// for the process of key conversion
+type keyConverter struct {
+	appid string
+}
+
+// convertKey takes at new datastore key type and
+// returns a old key type
+func (c *keyConverter) convertNewKeyFormatToOldKeyFormat(key *nds.Key) (*Key, error) {
+	// if key conerstion is not enabled return right away
+	if convKey == nil {
+		return nil, errors.New(errKeyConversion)
+	}
+	var pKey *Key
+	var err error
+	if key.Parent != nil {
+		pKey, err = convertNewKeyFormatToOldKeyFormat(key.Parent)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &Key{
+		intID:     key.ID,
+		kind:      key.Kind,
+		namespace: key.Namespace,
+		parent:    pKey,
+		stringID:  key.Name,
+		appID:     c.appid,
+	}, nil
+}
