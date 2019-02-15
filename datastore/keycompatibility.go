f4a94409e5f96724762d6c5def4832a4c13bc7d0
Luke Mccoy <lukemc@google.com>
2019-02-15 12:48:09 -0500

    fixed various comment nits and typos

--- datastore/keycompatibility.go
+++ datastore/keycompatibility.go
@@ -6,7 +6,7 @@
 // transition from google.golang.org/appengine/datastore (oldds) to cloud.google.com/go/datastore (newds).
 // Each lib (oldds and newds) contain functions Key.Encode() and Key.Decode(). These functions
 // create base64 representations of a json marshalled type of datastore keys.  Customers have been using
-// these encoded values to communicate between services in appengine.  The issue is that protobuf key types
+// these encoded values to communicate between services in appengine.  Protobuf key types
 // change between oldds and newds making the corresponding base64 key strings incompatible.
 // Customer who attempt to upgrade to newds that use this pattern will fail.
 // keycompatibility.go placed in oldds enables forward compatibility of newds encoded keys.
@@ -22,15 +22,14 @@ import (
 	"google.golang.org/appengine/internal"
 )
 
-var errKeyConversion string = "Key conversions must be enabled in the application.\n  See <link> for more details."
+var errKeyConversion string = `Key conversions must be enabled in the application.\n  
+See https://github.com/golang/appengine#key-encode-decode-compatibiltiy-to -help-with-datastore-library-migrations for more details.`
+
 var convKey *keyConverter
 
-// EnableKeyConversion enables forward key conversation
-// abilities.  Calling this function in a single handler
-// will enable the feature for all handlers.  This function can be called in
-// the /_ah/start handler.
-// Support for key converstion.  Variable holds the appid
-// so that key conversion can retrieve it without a context.
+// EnableKeyConversion enables forward key conversion abilities.  Calling this function in a single handler will enable
+// the feature for all handlers.  This function can be called in the /_ah/start handler.  Support for key converstion.
+// Variable holds the appid so that key conversion can retrieve it without a context.
 func EnableKeyConversion(ctx context.Context) {
 	convKey = &keyConverter{
 		appid: internal.FullyQualifiedAppID(ctx),
@@ -38,16 +37,14 @@ func EnableKeyConversion(ctx context.Context) {
 	return
 }
 
-// KeyConverter is the struct used to hold the appid
-// for the process of key conversion
+// keyConverter is the struct used to hold the appid for the process of key conversion
 type keyConverter struct {
 	appid string
 }
 
-// convertKey takes at new datastore key type and
-// returns a old key type
+// convertKey takes at new datastore key type and returns a old key type
 func (c *keyConverter) convertNewKeyFormatToOldKeyFormat(key *nds.Key) (*Key, error) {
-	// if key conerstion is not enabled return right away
+	// if key conversion is not enabled return right away
 	if c == nil {
 		return nil, errors.New(errKeyConversion)
 	}
