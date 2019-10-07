d3383a89d0ea484aa7991e26e89c5bc7baa71058
Julie Hockett <juliehockett@google.com>
2019-10-07 11:17:54 -0700

    Update testing SDK to latest version
    Release notes: https://cloud.google.com/appengine/docs/standard/go/release-notes

--- travis_install.sh
+++ travis_install.sh
@@ -9,7 +9,7 @@ fi
 
 if [[ $GOAPP == "true" ]]; then
   mkdir /tmp/sdk
-  curl -o /tmp/sdk.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip"
+  curl -o /tmp/sdk.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.71.zip"
   unzip -q /tmp/sdk.zip -d /tmp/sdk
   # NOTE: Set the following env vars in the test script:
   # export PATH="$PATH:/tmp/sdk/go_appengine"
