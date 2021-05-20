9b29304cafc79036ec5441fb6856a6622e3c41dc
Jason Collins <jason.a.collins@gmail.com>
2021-05-20 10:42:14 -0700

    Changes from code review.

--- internal/log.go
+++ internal/log.go
@@ -31,11 +31,11 @@ func logf(c *context, level int64, format string, args ...interface{}) {
 		panic("not an App Engine context")
 	}
 
-	if !IsSecondGen() {
+	if !IsStandard() {
 		s := strings.TrimRight(fmt.Sprintf(format, args...), "\n")
 		now := timeNow().UTC()
 		timestamp := fmt.Sprintf("%d/%02d/%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-		fmt.Fprintf(logStream, timestamp+" "+logLevelName[level]+": "+s+"\n")
+		fmt.Fprintf(logStream, "%s %s: %s\n", timestamp, logLevelName[level], s)
 		return
 	}
 
